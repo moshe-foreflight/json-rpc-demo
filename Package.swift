2e23ec9f48637492678ea3d3aca72140aa1710ec
Moshe <moshe@foreflight.com>
2024-03-06 12:10:41 -0500

    first commit

---
+++ Package.swift
@@ -0,0 +1,53 @@
+// swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-json-rpc",
+    products: [
+        .library(name: "JSONRPC", targets: ["JSONRPC"]),
+        .executable(name: "ServerExample", targets: ["ServerExample"]),
+        .executable(name: "ClientExample", targets: ["ClientExample"]),
+        .executable(name: "LightsdDemo", targets: ["LightsdDemo"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio", from: "2.42.0"),
+        .package(url: "https://github.com/apple/swift-nio-extras", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "JSONRPC",
+            dependencies: [
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "NIOFoundationCompat", package: "swift-nio"),
+                .product(name: "NIOExtras", package: "swift-nio-extras"),
+            ],
+            path: "Sources/JsonRpc"),
+        .executableTarget(
+            name: "ServerExample",
+            dependencies: [
+                "JSONRPC"
+            ]),
+        .executableTarget(
+            name: "ClientExample",
+            dependencies: [
+                "JSONRPC"
+            ]),
+        .executableTarget(
+            name: "LightsdDemo",
+            dependencies: [
+                "JSONRPC"
+            ]),
+        .testTarget(
+            name: "JSONRPCTests",
+            dependencies: [
+                "JSONRPC",
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            path: "Tests/JsonRpcTests"),
+    ]
+)
